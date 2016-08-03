26124cfcef03caff713bdf7609d0ac82e4bdf92f
Jack <chengxianga2008@yahoo.com>
2016-08-02 22:05:43 -0700

    Update

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'qtech');
+define('DB_NAME', 'i1482735_wp2');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'i1482735_wp2');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'i1482735_wp2');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
