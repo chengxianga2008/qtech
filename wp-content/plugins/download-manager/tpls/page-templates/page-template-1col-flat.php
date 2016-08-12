07d3cc200b97e20c8969b6cfb065741da5b3077a
Jack <chengxianga2008@yahoo.com>
2016-08-12 13:55:35 +0800

    Update

--- wp-content/plugins/download-manager/tpls/page-templates/page-template-1col-flat.php
+++ wp-content/plugins/download-manager/tpls/page-templates/page-template-1col-flat.php
@@ -1,33 +1,35 @@
-<!-- WPDM Template: Single Column, Flat -->
-<style type="text/css">.more_previews_a{display: block;float: left;margin-right: 6px;} .table{  max-width: 100%;  } .single-col-flat img{ box-shadow: none !important; border-radius: 0 !important; } </style>
-
+<!-- WPDM Template: Default Template -->
 <div class="row">
-<div class="col-md-12 single-col-flat">
-    [thumb_900x5000]
-    <br clear="all"/>
-    <br clear="all"/>
+<div class="col-md-12">
+
 <table class="table table-bordered">
 <tbody>
-<tr><td colspan="2">[thumb_gallery_40x40]</td></tr>
 <tr><td>Version</td><td>[version]</td></tr>
 <tr><td>Download</td><td>[download_count]</td></tr>
 <tr><td>Stock</td><td>[quota]</td></tr>
 <tr><td>Total Files</td><td>[file_count]</td></tr>
-<tr><td>Size</td><td>[file_size]</td></tr>
+<tr><td>File Size</td><td>[file_size]</td></tr>
 <tr><td>Create Date</td><td>[create_date]</td></tr>
-<tr><td>Last Updated</td><td>[update_date]</td></tr> 
-</tbody></table>    
-</div> 
+<tr><td>Last Updated</td><td>[update_date]</td>
+</tr>
+</tbody></table>
+
 </div>
-<div class="row">
 <div class="col-md-12">
-    [description]
-<br /> 
-[file_list] 
-<br/>
-[download_link]
+[description]  
+ 
+<br />
+<div class="panel panel-default">
+    <div class="panel-heading"><b>Download</b></div>
+    [file_list]
+    <div class="panel-footer text-right">
+        [download_link]
+    </div>
 </div>
 
+<br>
+
+</div> 
 </div>
 
 
